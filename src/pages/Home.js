bcccf0a493263515cb1296c7fd22a8e4ec7dec84
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-15 10:00:51 -0300

    separando button e input em componentes separados

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,10 @@ import {
     StyleSheet, 
     TextInput, 
     Platform,
-    TouchableOpacity
 } from "react-native"
+
+import { Button } from "../components/Button"
+import { SkillCard } from "../components/SkillCard"
 
 export function Home() {
     const [newSkill, setNewSkill] = useState('')
@@ -27,26 +29,14 @@ export function Home() {
             onChangeText={setNewSkill}
         />
 
-        <TouchableOpacity 
-            style={styles.button}
-            activeOpacity={0.7}
-            onPress={handleAddNewSkill}
-        >
-            <Text style={styles.buttonText}>Add</Text>
-        </TouchableOpacity>
+        <Button />
 
         <Text style={[styles.title, { marginVertical: 50 }]}>
             My Skills
         </Text>
         
         {
-            mySkills.map((skill, index) => (
-            <TouchableOpacity key={index} style={styles.buttonSkill}>
-                <Text style={styles.textSkill}>
-                {skill}
-                </Text>
-            </TouchableOpacity>
-            ))
+            mySkills.map((skill, index) => <SkillCard key={index} />)
         }
 
         </View>
@@ -72,29 +62,5 @@ const styles = StyleSheet.create({
         padding: Platform.OS === 'ios' ? 15 : 10,
         marginTop: 30,
         borderRadius: 12
-    },
-    button: {
-        backgroundColor: '#A370F7',
-        padding: 15,
-        borderRadius: 7,
-        alignItems: 'center',
-        marginTop: 20
-    },
-    buttonText: {
-        color: '#FFF',
-        fontSize: 17,
-        fontWeight: 'bold'
-    },
-    buttonSkill: {
-        backgroundColor: '#1F1E25',
-        padding: 15,
-        borderRadius: 50,
-        alignItems: 'center',
-        marginVertical: 10
-    },
-    textSkill: {
-        color: '#FFF',
-        fontSize: 22,
-        fontWeight: 'bold'
     }
 })
