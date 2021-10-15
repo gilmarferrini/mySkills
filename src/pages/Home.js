a8ba609d3d0ae0f9d2d0ea0b3e25524592b20981
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-15 09:52:58 -0300

    adicionado os estados com o useState

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,69 +1,100 @@
-import React from "react"
+import React, { useState } from "react"
 import { 
-  View, 
-  Text, 
-  StyleSheet, 
-  TextInput, 
-  Platform,
-  TouchableOpacity
+    View, 
+    Text, 
+    StyleSheet, 
+    TextInput, 
+    Platform,
+    TouchableOpacity
 } from "react-native"
 
 export function Home() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome, Gilmar</Text>
+    const [newSkill, setNewSkill] = useState('')
+    const [mySkills, setMySkills] = useState([])
 
-      <TextInput 
-        style={styles.input}
-        placeholder="New skill"
-        placeholderTextColor="#555"
-      />
+    function handleAddNewSkill() {
+        setMySkills(oldState => [...oldState, newSkill])
+    }
 
-      <TouchableOpacity 
-        style={styles.button}
-        activeOpacity={0.7}
-      >
-        <Text style={styles.buttonText}>Add</Text>
-      </TouchableOpacity>
+    return (
+        <View style={styles.container}>
+        <Text style={styles.title}>Welcome, Gilmar</Text>
 
-      <Text style={[styles.title, { marginTop: 50 }]}>
-        My Skills
-      </Text>
+        <TextInput 
+            style={styles.input}
+            placeholder="New skill"
+            placeholderTextColor="#555"
+            onChangeText={setNewSkill}
+        />
 
-    </View>
-  )
+        <TouchableOpacity 
+            style={styles.button}
+            activeOpacity={0.7}
+            onPress={handleAddNewSkill}
+        >
+            <Text style={styles.buttonText}>Add</Text>
+        </TouchableOpacity>
+
+        <Text style={[styles.title, { marginVertical: 50 }]}>
+            My Skills
+        </Text>
+        
+        {
+            mySkills.map((skill, index) => (
+            <TouchableOpacity key={index} style={styles.buttonSkill}>
+                <Text style={styles.textSkill}>
+                {skill}
+                </Text>
+            </TouchableOpacity>
+            ))
+        }
+
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    backgroundColor: '#121015',
-    paddingVertical: 70,
-    paddingHorizontal: 30
-  },
-  title: {
-    color: '#FFF',
-    fontSize: 24,
-    fontWeight: 'bold'
-  },
-  input: {
-    backgroundColor: '#1F1E25',
-    color: '#FFF',
-    fontSize: 18,
-    padding: Platform.OS === 'ios' ? 15 : 10,
-    marginTop: 30,
-    borderRadius: 12
-  },
-  button: {
-    backgroundColor: '#A370F7',
-    padding: 15,
-    borderRadius: 7,
-    alignItems: 'center',
-    marginTop: 20
-  },
-  buttonText: {
-    color: '#FFF',
-    fontSize: 17,
-    fontWeight: 'bold'
-  }
+    container: {
+        flex: 1, 
+        backgroundColor: '#121015',
+        paddingVertical: 70,
+        paddingHorizontal: 30
+    },
+    title: {
+        color: '#FFF',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    input: {
+        backgroundColor: '#1F1E25',
+        color: '#FFF',
+        fontSize: 18,
+        padding: Platform.OS === 'ios' ? 15 : 10,
+        marginTop: 30,
+        borderRadius: 12
+    },
+    button: {
+        backgroundColor: '#A370F7',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 17,
+        fontWeight: 'bold'
+    },
+    buttonSkill: {
+        backgroundColor: '#1F1E25',
+        padding: 15,
+        borderRadius: 50,
+        alignItems: 'center',
+        marginVertical: 10
+    },
+    textSkill: {
+        color: '#FFF',
+        fontSize: 22,
+        fontWeight: 'bold'
+    }
 })
