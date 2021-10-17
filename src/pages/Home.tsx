be3fc68073901051d1d069f02771ae5a3d843ef2
gilmarferrini <ferrinigilmar@gmail.com>
2021-10-17 09:07:37 -0300

    tipando SkillCard e adicionando funcao para remover SkillCard

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,6 +30,12 @@ export function Home() {
         setMySkills(oldState => [...oldState, data])
     }
 
+    function handleRemoveSkill(id: string) {
+        setMySkills(oldState => mySkills.filter(
+            skill => skill.id !== id
+        ))
+    }
+
     useEffect(() => {
         const currentHour = new Date().getHours();
         
@@ -69,7 +75,12 @@ export function Home() {
         <FlatList 
             data={mySkills}
             keyExtractor={item => item.id}
-            renderItem={({ item }) => <SkillCard skill={item} />}
+            renderItem={({ item }) => (
+                <SkillCard 
+                    skill={item.name}
+                    onPress={() => handleRemoveSkill(item.id)}
+                />
+            )}
         />
 
         </View>
